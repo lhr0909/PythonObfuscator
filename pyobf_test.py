840f92619b79f252be9d20156278a95473614d3a
Liang, Simon <hlliang@amazon.com>
2014-03-11 16:23:10 -0700

    rolled back and currently this only supports python 2

--- pyobf_test.py
+++ pyobf_test.py
@@ -54,4 +54,9 @@ class PyObfSpec(unittest.TestCase):
 if __name__ == "__main__":
     #unittest.main()
     suite = unittest.TestLoader().loadTestsFromTestCase(PyObfSpec)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    test_result = unittest.TextTestRunner(verbosity=2).run(suite)
+    if test_result.wasSuccessful():
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
